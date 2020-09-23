884313e617e15c3de9deb3f77041f98c9ed5b76d
tina.zhang <tina.zhang@rccchina.com>
2020-09-23 19:14:29 +0800

    缓存优化

--- src/cache/index.vue
+++ src/cache/index.vue
@@ -16,33 +16,40 @@ import { mapActions, mapState } from 'vuex'
 import cache from '../lib/func/cache'
 
 export default {
-  data() {
-      return {
-          show_category_tree_flag: false,
-          spinShow: false,
-          catsData: [],
-      }
+  data () {
+    return {
+      show_category_tree_flag: false,
+      spinShow: false,
+      catsData: []
+    }
   },
   methods: {
     ...mapActions('categroy', ['getCate']),
     // onShowCategoryTree() {
     //   this.getCate()
     // }
-    onShowCategoryTree() {
-      cache.checkActive("cacheType")
+    onShowCategoryTree () {
+      let _key = 'CATEGORY'
+      cache.checkActive(_key).then(res => {
+        console.log(res)
+        if (res) {
+          cache.get(_key).then(data => { this.catsData = data })
+        }
+      })
 
       this.show_category_tree_flag = true
       this.spinShow = true
       // 取得全部分类
       categoryTreeShow().then(response => {
-          let data = response.data.data
-          this.catsData = data.categroy_tree
-          this.spinShow = false
+        let data = response.data.data
+        this.catsData = data.categroy_tree
+        this.spinShow = false
+        cache.set(_key, data.categroy_tree)
       })
     }
   },
   computed: {
     // ...mapState('categroy', ['catsData', 'spinShow'])
-  },
+  }
 }
 </script>
